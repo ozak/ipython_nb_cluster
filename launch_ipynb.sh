a994eafdb7565f192527960d358addc366ee84d4
Ömer Özak <ozak@users.noreply.github.com>
2015-01-16 16:50:48 -0600

    version 2.1

--- launch_ipynb.sh
+++ launch_ipynb.sh
@@ -6,14 +6,14 @@
 #SBATCH --mem-per-cpu=1024
 
 echo "Launching controller"
-$HOME/anaconda/bin/ipcontroller --ip='*' &
+ipcontroller --ip='*' &
 sleep 10
 
 echo "Launching engines"
-srun $HOME/anaconda/bin/ipengine &
+ipengine &
 sleep 25
 
 echo "Launching job"
-$HOME/anaconda/bin/python checkipnb.py $1
+python checkipnb.py $1
 
 echo "Done!"
